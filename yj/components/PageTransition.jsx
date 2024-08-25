7df2a60621ccb3db012cd48966a34ccb28aef5d1
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-08-25 14:35:34 +0530

    Basic Ui Completed

--- yj/components/PageTransition.jsx
+++ yj/components/PageTransition.jsx
@@ -1,10 +1,18 @@
+"use client";
+import { AnimatePresence, delay, motion } from "framer-motion";
 import React from 'react'
+import { usePathname } from "next/navigation";
 
 const PageTransition = ({children}) => {
+  const pathName = usePathname();
   return (
     <AnimatePresence>
+        <div ey = {pathName}>
+          <motion.div className="h-screen w-screen fixed bg-primary top-0 pointer-events-none" initial={{opacity: 1}} animate={{opacity: 0, transition: {delay:1, duration: 0.4, ease: 'easeInOut' }}} 
+           />
         {children}
-        HI
+      
+        </div>
     </AnimatePresence>
   )
 }
