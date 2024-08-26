eaaf15c89c8460b80a11fcd118676aaa4dd4e88f
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-08-26 12:55:28 +0530

    Creating services page

--- yj/app/services/page.jsx
+++ yj/app/services/page.jsx
@@ -2,18 +2,50 @@
 
 import { BsArraowDownRight } from "react-icons/bs";
 import Link from "next/link";
-import React from 'react'
+import React from 'react';
+import { motion } from "framer-motion";
 
 const services = [
   {
     num: '01',
     title: 'Web Development',
-    description: ""
+    description: "I create dynamic, responsive websites using React, Next.js, Node.js, and Django. I'm also flexible to switch stacks according to project requirements, ensuring the best possible solution for your needs.",
+    href: "/"
+  },
+  {
+    num: '02',
+    title: 'AI/ML',
+    description: "I develop intelligent solutions and predictive models using technologies like TensorFlow, and scikit-learn. Whether it's data analysis, machine learning, or deploying AI-driven applications, I tailor my approach to meet your specific project requirements",
+    href:"/"
+  },
+  {
+    num: '03',
+    title: 'Blockchain',
+    description: "I'm new to blockchain technology but actively learning to build secure, decentralized applications using this innovative technology.",
+    href:""
   }
 ]
 const Services = () => {
   return (
-    <div>services page</div>
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto"> 
+        <motion.div initial={{opacity:0}} animate={{opacity: 1, transition: {delay:2.4, duration:0.4, ease:'easeIn'} }}>
+          {
+            services.map((service, index) =>{
+              return <div key={index}>
+                <div>
+                  <div>{service.num}</div>
+                  <Link href={service.href}>
+                    <BsArraowDownRight />
+                  </Link>
+                </div>
+              </div>
+            })
+          }
+
+        </motion.div>
+      </div>
+    </section>
   )
 }
 
--- yj/components/Stats.jsx
+++ yj/components/Stats.jsx
@@ -19,6 +19,10 @@ const stats = [
         num: 350,
         text: "Code commits", 
     },
+    {
+        num: 125,
+        text: "Problems solved"
+    }
 
 ]
 const Stats = () => {
@@ -28,7 +32,7 @@ const Stats = () => {
             <div className='flex gap-6 flex-wrap max-w-[80vw] mx-auto xl:max-w-none '>
             {stats.map((item, index)=>{
                 return <div key={index} className='flex-1 flex gap-4 items-center justify-center xl:justify-start'>
-                    <CountUp end={item.num} duration={5} delay={2} className='text-4xl xl:text-6xl font-extrabold' /> <p className='text-4xl xl:text-6xl font-extrabold'>{index === 0 ? "": "+"}</p>
+                    <CountUp end={item.num} duration={10} delay={2} className='text-3xl xl:text-4xl font-extrabold' /> <p className='text-4xl xl:text-6xl font-extrabold'>{index === 0 ? "": "+"}</p>
                 <p className={`${item.text.length <15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
                 </div>
                 
