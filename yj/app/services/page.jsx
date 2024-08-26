9dc03249c8094f1b0bbc443b2b4e95cc543e42ea
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-08-26 11:35:15 +0530

    home page completed

--- yj/app/services/page.jsx
+++ yj/app/services/page.jsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { BsArraowDownRight } from "react-icons/bs";
+import Link from "next/link";
 import React from 'react'
 
+const services = [
+  {
+    num: '01',
+    title: 'Web Development',
+    description: ""
+  }
+]
 const Services = () => {
   return (
     <div>services page</div>
