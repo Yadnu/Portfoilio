912cfd1e9293d5842716c1fc528a8bf3f63f88fe
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-08-26 14:20:46 +0530

    resume section

--- yj/app/resume/page.jsx
+++ yj/app/resume/page.jsx
@@ -1,5 +1,21 @@
+"use client";
 import React from 'react'
-
+import {TbBrandcpp} from 'react-icons/tb';
+import { FaHtml5,FaReact,FaTs,FaFigma, FaNodeJs, FaJs } from 'react-icons/fa';
+import{
+  SiTailwindcss, SiNextdotjs
+} from "react-icons/si";
+// about data
+const about = {
+  title: 'About me',
+  description: "",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Yadneya Joshi"
+    }
+  ]
+};
 const Resume = () => {
   return (
     <div>Resume</div>
