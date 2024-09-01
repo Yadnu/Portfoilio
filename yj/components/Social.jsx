31786667dacc535730b801ae0595a06e4ead71be
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-09-01 07:30:02 +0530

    Added link fixes

--- yj/components/Social.jsx
+++ yj/components/Social.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { FaGithub, FaLinkedin, FaMedium, FaInstagram, FaTwitter} from 'react-icons/fa';
 
 const socials = [
-    {icon: <FaGithub/>, path: ''},
-    {icon: <FaLinkedin/>, path: ''},
-    {icon: <FaMedium/>, path: ''},
-    {icon: <FaInstagram/>, path: ''},
-    {icon: <FaTwitter/>, path: ''},
+    {icon: <FaGithub/>, path: 'https://github.com/Yadnu'},
+    {icon: <FaLinkedin/>, path: 'https://www.linkedin.com/in/yadneya-joshi-920299198/'},
+    {icon: <FaMedium/>, path: 'https://medium.com/@yadneyajoshi3'},
+    {icon: <FaInstagram/>, path: 'https://www.instagram.com/yadneya_joshi_/'},
+    {icon: <FaTwitter/>, path: 'https://x.com/tecob207'},
 
 ]
 const Social = ({containerStyles, iconStyles}) => {
