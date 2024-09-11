6e7d553cef04b029753e757ee1c2ce57067f1381
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-09-11 13:37:32 +0530

    Added contact me

--- yj/app/contact/page.jsx
+++ yj/app/contact/page.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
         <div className='flex flex-col xl:flex-row gap-[30px]'>
           {/* form */}
         <div className='xl:w-[54%] order-2 xl:order-none'>
-        <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
+        <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl' method='POST' action="https://formspree.io/f/yadneyejoshi3@gmail.com">
           <h3 className='text-4xl text-accent'>Let's work together</h3>
           <p className='text-white/60'>
             If you wish to hire or collaborate with me, feel free to feel the form.
@@ -70,7 +70,7 @@ const Contact = () => {
             placeholder="Type your message here."
           />
           {/* Btn */}
-          <Button size="md" className="max-w-40">
+          <Button size="md" className="max-w-40" type="submit">
             Send message
           </Button>
         </form>
